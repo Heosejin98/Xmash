3009d7d755ead955015da8caac2c44326d981d42
sejin <sejin3319@naver.com>
2024-08-01 19:58:39 +0900

    [be] <config> init

---
+++ be/xmash/src/main/java/com/tmp/xmash/XmashApplication.java
@@ -0,0 +1,13 @@
+package com.tmp.xmash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmashApplication.class, args);
+	}
+
+}
---
+++ be/xmash/src/test/java/com/tmp/xmash/XmashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmp.xmash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
