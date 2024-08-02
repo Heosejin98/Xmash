90e3f21e0d5376dc6694c6df5d394b01f45b0f75
sejin <sejin3319@naver.com>
2024-08-03 00:22:38 +0900

    [#1] backend ci-cd 설정 test

--- be/xmash/src/main/java/com/tmp/xmash/XmashApplication.java
+++ be/xmash/src/main/java/com/tmp/xmash/XmashApplication.java
@@ -3,7 +3,7 @@ package com.tmp.xmash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//tes
+//test
 @SpringBootApplication
 public class XmashApplication {
 
