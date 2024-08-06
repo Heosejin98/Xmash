1b5978c662650e776fa2f9da5a8aa923eaae0a3b
김진범 <59330110+jinbekim@users.noreply.github.com>
2024-08-06 19:43:40 +0900

    chore: Update dependencies and remove unused code (#7)

--- fe/src/pages/login.lazy.tsx
+++
@@ -1,4 +0,0 @@
-import { LoginForm } from "@/widgets/login/loginForm.ui";
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/login")({ component: () => <LoginForm /> });
---
+++ fe/src/pages/login.tsx
@@ -0,0 +1,17 @@
+import { LoginForm } from "@/widgets/login/loginForm.ui";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { z } from "zod";
+
+const fallback = "/" as const;
+
+export const Route = createFileRoute("/login")({
+  validateSearch: z.object({
+    redirect: z.string().optional().catch(""),
+  }),
+  beforeLoad: ({ context, search }) => {
+    if (context.isAuthenticated) {
+      throw redirect({ to: search.redirect || fallback });
+    }
+  },
+  component: () => <LoginForm />,
+});
