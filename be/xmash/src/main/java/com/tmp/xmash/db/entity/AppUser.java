bf8dba827f557cb05c5e01be3b646624d56a3d2c
sejin <61305083+Heosejin98@users.noreply.github.com>
2024-08-12 21:11:23 +0900

    [be-#3] <feat> login (#8)

---
+++ be/xmash/src/main/java/com/tmp/xmash/db/entity/AppUser.java
@@ -0,0 +1,30 @@
+package com.tmp.xmash.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@ToString
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "app_user_id")
+    private Long app_user_id;
+    private String userId;
+    private String email;
+    private String name;
+    private String password;
+    private String gender;
+
+    protected AppUser() {}
+
+}
