80e96f322607184f2f6e44edd4a0ce6450fceaef
sejin <61305083+Heosejin98@users.noreply.github.com>
2024-08-30 19:58:40 +0900

    [be-#3] <feat> login (#10)

--- be/xmash/src/main/java/com/tmp/xmash/db/repositroy/UserRepository.java
+++ be/xmash/src/main/java/com/tmp/xmash/db/repositroy/UserRepository.java
@@ -1,10 +1,12 @@
 package com.tmp.xmash.db.repositroy;
 
 import com.tmp.xmash.db.entity.AppUser;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
+    Optional<AppUser> findByUserId(String userId);
 }
---
+++ be/xmash/src/main/java/com/tmp/xmash/service/LoginService.java
@@ -0,0 +1,34 @@
+package com.tmp.xmash.service;
+
+import com.tmp.xmash.db.entity.AppUser;
+import com.tmp.xmash.db.repositroy.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Transactional
+    public String login(String userId, String password) {
+        AppUser appUser = userRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        if (!passwordEncoder.matches(password, appUser.getPassword())) {
+            throw new IllegalArgumentException("Password not matched");
+        }
+
+        return passwordEncoder.encode(userId);
+    }
+
+    @Transactional
+    public void signUp(String userId, String password) {
+        userRepository.save(new AppUser(userId, passwordEncoder.encode(password)));
+    }
+}
