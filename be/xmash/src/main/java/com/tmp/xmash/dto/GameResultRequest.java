0e697e4b41a016a1b62a108541b3a4b79ff51a02
Heosejin98 <sejin3319@naver.com>
2024-09-09 22:11:57 +0900

    [#19] SwaggerConfig add

--- be/xmash/src/main/java/com/tmp/xmash/dto/GameResultRequest.java
+++ be/xmash/src/main/java/com/tmp/xmash/dto/GameResultRequest.java
@@ -21,8 +21,8 @@ public class GameResultRequest {
      * 내 점수
      * @return myScore
      */
-    @Schema(name = "myScore", example = "15", description = "내 점수", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-    @JsonProperty("myScore")
+    @Schema(name = "homeScore", example = "15", description = "내 점수", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @JsonProperty("homeScore")
     public Integer getHomeScore() {
         return homeScore;
     }
