9c517389c368dc9ada043fa4d0d0b92f1ee806fa
김진범 <jinbekim@ex-em.com>
2024-09-27 22:48:54 +0900

    Revert "chor: test"
    This reverts commit af32d1af0e92428d3242124b414cbd32a2530de5.

--- fe/src/shared/api/index.ts
+++ fe/src/shared/api/index.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   proxy: false,
-  // withCredentials: true,
+  withCredentials: true,
 })
 
 export function handleGenericError(error: AxiosError) {
