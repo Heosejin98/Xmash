bbc244d26159c83b3904eb2bceea5fd8f7400998
sejin <sejin3319@naver.com>
2024-09-27 18:49:07 +0900

    Revert "feat : allowOrigins config"
    This reverts commit 00e30442cc9d490572f16eefddac98c29bab4e11.

--- be/xmash/src/main/java/com/tmp/xmash/config/WebConfig.java
+++ be/xmash/src/main/java/com/tmp/xmash/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.tmp.xmash.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,17 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
-    @Value("${cors.allowed.origins}")
-    private String allowedOrigins;
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigins)
+                        .allowedOrigins("http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // 자격증명 허용
