0041d74ec21531683d33406c10b63a31678579f9
Heosejin98 <sejin3319@naver.com>
2024-09-28 17:16:27 +0900

    config : https로 변경

--- be/xmash/src/main/java/com/tmp/xmash/config/WebConfig.java
+++ be/xmash/src/main/java/com/tmp/xmash/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Value("${cors.allowed.origins:http://xmash.com}")
+    @Value("${cors.allowed.origins:http://localhost.com}")
     private String[] allowedOrigins;
 
     @Bean
