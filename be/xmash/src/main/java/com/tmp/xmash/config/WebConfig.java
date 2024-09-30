0c911a305cbbf770151bde317f1f26a3bcc24d58
sejin <sejin3319@naver.com>
2024-09-30 19:20:10 +0900

    config : localhost allow add

--- be/xmash/src/main/java/com/tmp/xmash/config/WebConfig.java
+++ be/xmash/src/main/java/com/tmp/xmash/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.tmp.xmash.config;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +15,14 @@ public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+        String[] tmp = Arrays.copyOf(allowedOrigins, allowedOrigins.length + 1);
+        tmp[allowedOrigins.length] = "http://localhost.com";
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigins)
+                        .allowedOrigins(tmp)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // 자격증명 허용
