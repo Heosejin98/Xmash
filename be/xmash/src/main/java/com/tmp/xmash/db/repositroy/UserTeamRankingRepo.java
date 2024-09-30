7bc1df782301698ffa3a859cf0255598e3a17889
sejin <61305083+Heosejin98@users.noreply.github.com>
2024-09-30 19:58:21 +0900

    [BE] feat : team 조회 기능  (#66)
    * config : localhost allow add
    
    * feat : 나의 팀 정보 조회

--- be/xmash/src/main/java/com/tmp/xmash/db/repositroy/UserTeamRankingRepo.java
+++ be/xmash/src/main/java/com/tmp/xmash/db/repositroy/UserTeamRankingRepo.java
@@ -1,11 +1,13 @@
 package com.tmp.xmash.db.repositroy;
 
 import com.tmp.xmash.db.entity.UserTeamRanking;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserTeamRankingRepo extends JpaRepository<UserTeamRanking, Integer> {
 
+    Set<UserTeamRanking> findByTeamUserId(Long teamUserId);
 
 }
