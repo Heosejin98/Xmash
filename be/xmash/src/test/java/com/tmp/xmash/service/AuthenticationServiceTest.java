6f4d711bb7f58e1ba194bbf86716290f32cc298b
Heosejin98 <sejin3319@naver.com>
2024-10-05 23:34:30 +0900

    feat : 임시 회원가입 로직 변경

--- be/xmash/src/test/java/com/tmp/xmash/service/AuthenticationServiceTest.java
+++ be/xmash/src/test/java/com/tmp/xmash/service/AuthenticationServiceTest.java
@@ -1,5 +1,6 @@
 package com.tmp.xmash.service;
 
+import com.tmp.xmash.type.Gender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,11 +14,18 @@ class AuthenticationServiceTest {
     @Test
     void signUp() {
         // given...
-        String userId = "se1";
-        String password = "qwe123";
+        String userId = "bgm";
+        String password = "bgm";
+        Gender gender = Gender.MALE;
+        String name = "유근모";
+        String email = "bgm@ex-em.com";
 
         // when
-//        loginService.signUp(userId, password);
-        // then
+        loginService.signUp(userId,
+                password,
+                email,
+                name,
+                gender
+        );
     }
 }
