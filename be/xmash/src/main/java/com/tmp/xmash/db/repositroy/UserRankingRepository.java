10d319e2a8dee4e8fa0c769eb6a5ae3d3648c6a1
sejin <61305083+Heosejin98@users.noreply.github.com>
2024-10-05 23:37:34 +0900

    Revert "[BE] 회원가입 버그 수정 #77  (#78)"
    This reverts commit d90aa486dfd2bcaa7deb7338d72e7c7881fdf67f.

--- be/xmash/src/main/java/com/tmp/xmash/db/repositroy/UserRankingRepository.java
+++ be/xmash/src/main/java/com/tmp/xmash/db/repositroy/UserRankingRepository.java
@@ -2,10 +2,9 @@ package com.tmp.xmash.db.repositroy;
 
 
 import com.tmp.xmash.db.entity.UserRanking;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UserRankingRepository extends JpaRepository<UserRanking, Long> {
