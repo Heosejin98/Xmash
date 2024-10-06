c87feff5d9f446fefc1fe1fd862948bfcdaf3be6
sejin <61305083+Heosejin98@users.noreply.github.com>
2024-10-06 16:42:42 +0900

    [BE] 경기등록 유효성 검증 추가 (#82)
    * fix: production api
    
    * fix api path
    
    * feat : 경기등록 유효성 검증 추가
    
    ---------
    
    Co-authored-by: 김진범 <lljrwq123@gmail.com>

--- be/xmash/src/main/java/com/tmp/xmash/exption/GlobalControllerExceptionHandler.java
+++ be/xmash/src/main/java/com/tmp/xmash/exption/GlobalControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import com.tmp.xmash.dto.response.GlobalExceptionResponse;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.log4j.Log4j2;
 import org.apache.coyote.BadRequestException;
+import org.apache.tomcat.websocket.AuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,6 +19,14 @@ public class GlobalControllerExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadRequestException.class)
     private GlobalExceptionResponse handleBadRequestException(BadRequestException e) {
+        log.error("Bad Request Exception : {}", e.getMessage());
+        return new GlobalExceptionResponse(e.getMessage());
+    }
+
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(AuthenticationException.class)
+    private GlobalExceptionResponse handleAuthenticationException(AuthenticationException e) {
         log.error("Bad Request Exception : {}", e.getMessage());
         return new GlobalExceptionResponse(e.getMessage());
     }
