badbfe5d9cda873b56b2a24dbbcbcd4ebb7f68ef
Heosejin98 <sejin3319@naver.com>
2024-10-07 22:26:59 +0900

    feat : logout add

--- be/xmash/src/main/java/com/tmp/xmash/web/AuthenticationController.java
+++ be/xmash/src/main/java/com/tmp/xmash/web/AuthenticationController.java
@@ -4,6 +4,9 @@ import com.tmp.xmash.dto.request.LoginRequest;
 import com.tmp.xmash.dto.response.UserProfileResponse;
 import com.tmp.xmash.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,11 +37,27 @@ public class AuthenticationController {
         }
     }
 
-
     @PostMapping("/logout")
     public ResponseEntity<Boolean> logoutPost(
-            HttpSession session
+            HttpSession session,
+            HttpServletRequest request,
+            HttpServletResponse response
     ) {
+        if (session != null) {
+            // 세션 무효화: 세션 ID 삭제 및 모든 세션 데이터 제거
+            session.invalidate();
+        }
+
+        // 모든 쿠키 삭제
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                // 쿠키 삭제: 유효기간 0으로 설정
+                cookie.setMaxAge(0);
+                cookie.setPath("/"); // 애플리케이션 전체에서 쿠키 삭제
+                response.addCookie(cookie);
+            }
+        }
 
         return ResponseEntity.ok(Boolean.TRUE);
     }
