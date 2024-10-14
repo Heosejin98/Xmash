a6d248c9e08f811d1beece63055584ed9b563a54
김진범 <59330110+jinbekim@users.noreply.github.com>
2024-10-14 15:01:12 +0900

    refactor: Simplify validation rules for LoginUserDto (#92)
    Co-authored-by: 김진범 <lljrwq123@gmail.com>

--- fe/src/shared/api/auth/auth.contracts.ts
+++ fe/src/shared/api/auth/auth.contracts.ts
@@ -1,14 +1,8 @@
 import { z } from "zod";
 
 export const LoginUserDto = z.object({
-  userId: z
-    .string()
-    .min(2, {
-      message: "UserId must be at least 2 characters.",
-    }),
-  password: z.string().min(4, {
-    message: "Password must be at least 4 characters.",
-  }),
+  userId: z.string(),
+  password: z.string(),
 })
 export type LoginUserDto = z.infer<typeof LoginUserDto>
 
