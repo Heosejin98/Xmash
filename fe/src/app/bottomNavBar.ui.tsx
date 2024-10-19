9c96fa9dba6e4868d1fccb845e756481c937b393
김진범 <lljrwq123@gmail.com>
2024-10-19 13:43:08 +0900

    feat: 💫 add bounce animation on active menu

--- fe/src/app/bottomNavBar.ui.tsx
+++ fe/src/app/bottomNavBar.ui.tsx
@@ -2,6 +2,7 @@ import { useAuthStore } from "@/entities/user/user.store";
 import { AddGameButton } from "@/features/game/add-game.ui";
 import { Link } from "@tanstack/react-router";
 import { CSSProperties } from "react";
+import { Gamepad2, Medal } from "lucide-react";
 
 const activeProps: {
   style: CSSProperties;
@@ -16,14 +17,15 @@ const RankingLink = () => {
   return (
     <Link
       to="/ranking"
-      className="nav-link w-14"
+      className="nav-link  w-14 h-14 flex flex-col items-center group"
       search={{
         matchType: "single",
       }}
       activeOptions={{ includeSearch: false }}
       activeProps={activeProps}
     >
-      Ranking
+      <Medal className="group-data-[status=active]:animate-bounce" />
+      <span className="text-xs">순위</span>
     </Link>
   );
 };
@@ -32,7 +34,7 @@ const GameLink = () => {
   return (
     <Link
       to="/game"
-      className="nav-link w-14"
+      className="nav-link  w-14 h-14 flex flex-col items-center group"
       activeOptions={{ includeSearch: false }}
       activeProps={activeProps}
       search={{
@@ -40,7 +42,8 @@ const GameLink = () => {
         matchType: "all",
       }}
     >
-      Game
+      <Gamepad2 className="group-data-[status=active]:animate-bounce" />
+      <span className="text-xs">전적</span>
     </Link>
   );
 };
@@ -48,7 +51,7 @@ const GameLink = () => {
 const BottomNavBar = () => {
   const store = useAuthStore();
   return (
-    <nav className="flex justify-between p-4 sticky left-0 bottom-0 w-full h-nav z-10 bg-muted">
+    <nav className="flex justify-between p-4 left-0 bottom-0 w-full h-nav z-10 bg-muted fixed">
       <RankingLink />
       {store.isAuthenticated && <AddGameButton></AddGameButton>}
       <GameLink />
--- fe/src/pages/_layout.tsx
+++ fe/src/pages/_layout.tsx
@@ -5,11 +5,13 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 export const Route = createFileRoute("/_layout")({
   component: () => (
     <>
-      <Header></Header>
-      <main className="p-2 flex-1">
-        <Outlet />
-      </main>
-      <BottomNavBar></BottomNavBar>
+      <div>
+        <Header></Header>
+        <main className="p-2 flex-1 mb-nav">
+          <Outlet />
+        </main>
+        <BottomNavBar></BottomNavBar>
+      </div>
     </>
   ),
 });
