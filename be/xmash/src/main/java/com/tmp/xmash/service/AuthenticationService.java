d34af570327bec50f74ebf8bde1ff70a22a208ab
sejin <61305083+Heosejin98@users.noreply.github.com>
2024-10-21 20:07:37 +0900

    feat: 비밀번호 변경&로그인 에러 메시지 표출 (#120)
    * feat: 비밀번호 변경&로그인 에러 메시지 표출
    
    * feat: 비밀번호 변경&로그인 에러 메시지 표출

--- be/xmash/src/main/java/com/tmp/xmash/service/AuthenticationService.java
+++ be/xmash/src/main/java/com/tmp/xmash/service/AuthenticationService.java
@@ -24,11 +24,13 @@ public class AuthenticationService {
 
     @Transactional
     public UserProfileResponse login(String userId, String password) throws BadRequestException {
-        AppUser appUser = userRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+        AppUser appUser = userRepository.findByUserId(userId).orElseThrow(() -> new BadRequestException("비밀번호 또는 Id가 일치하지않습니다."));
 
         if (!passwordEncoder.matches(password, appUser.getPassword())) {
-            throw new BadRequestException("비밀번호가 일치하지 않습니다");
-        }
+            throw new BadRequestException("비밀번호 또는 Id가 일치하지않습니다.");
+        AppUser appUser = userRepository.findByUserId(userId).orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+
 
         return new UserProfileResponse(appUser.getUserId(),
                 appUser.getName(),
