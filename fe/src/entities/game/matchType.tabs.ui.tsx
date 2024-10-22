a5450e13ff7b6808764b2af80d7ed3f886e06182
김진범 <lljrwq123@gmail.com>
2024-10-22 23:45:33 +0900

    refactor: game tab

--- fe/src/entities/game/matchType.tabs.ui.tsx
+++ fe/src/entities/game/matchType.tabs.ui.tsx
@@ -10,7 +10,7 @@ export const MatchTypeTabs = ({ type, onChange }: MatchTypeTabsProps) => {
   return (
     <Tabs value={type} className="w-full" onValueChange={(type) => onChange(type as MatchType)}>
       <TabsList className="grid grid-cols-3">
-        <TabsTrigger value="all">전부 보여줘</TabsTrigger>
+        <TabsTrigger value="all">전체</TabsTrigger>
         <TabsTrigger value="single">단식</TabsTrigger>
         <TabsTrigger value="double">복식</TabsTrigger>
       </TabsList>
