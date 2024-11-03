4120c569a2661f9377fa2abe5c37b68bd7d3984b
김진범 <lljrwq123@gmail.com>
2024-11-03 13:24:11 +0900

    feat: ✨ add app version to environment and profile page

--- fe/src/pages/_auth/profile/index.tsx
+++ fe/src/pages/_auth/profile/index.tsx
@@ -1,8 +1,7 @@
 import { useLogout } from "@/features/login/logout.hook";
-import { Button, CardContent, CardDescription, CardHeader, CardTitle } from "@/shared/ui";
+import { Button, Card, CardContent, CardHeader, CardTitle } from "@/shared/ui";
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { LogOut } from "lucide-react";
-import { Card } from "@/shared/ui";
 
 export const Route = createFileRoute("/_auth/profile/")({
   component: Profile,
@@ -15,8 +14,7 @@ function Profile() {
     <>
       <Card className="w-4/5 m-auto">
         <CardHeader>
-          <CardTitle>Profile</CardTitle>
-          <CardDescription>Profile description</CardDescription>
+          <CardTitle>설정</CardTitle>
         </CardHeader>
         <CardContent>
           <nav className="space-y-1">
@@ -37,6 +35,9 @@ function Profile() {
               <LogOut />
             </Button>
           </nav>
+          <p className="mt-10 text-right text-xs text-gray-400">
+            앱 버전 {import.meta.env.VITE_APP_VERSION}
+          </p>
         </CardContent>
       </Card>
     </>
