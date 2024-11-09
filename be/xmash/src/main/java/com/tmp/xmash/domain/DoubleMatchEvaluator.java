418aad6374eb16500e7b5ea7b2c75c69d0409f0b
sejin <61305083+Heosejin98@users.noreply.github.com>
2024-11-09 23:31:58 +0900

    feat: 경기 수정 기능 완료 (#164)
    * refactor: MatchEvaluator refactoring
    
    * feat: 경기 수정 기능 완료

--- be/xmash/src/main/java/com/tmp/xmash/domain/DoubleMatchEvaluator.java
+++ be/xmash/src/main/java/com/tmp/xmash/domain/DoubleMatchEvaluator.java
@@ -1,66 +1,63 @@
 package com.tmp.xmash.domain;
 
-import static com.tmp.xmash.common.AppConstants.DEFAULT_WINNER_LP;
-import static com.tmp.xmash.common.AppConstants.RANDOM_GENERATOR;
-
+import com.tmp.xmash.db.entity.AppUser;
 import com.tmp.xmash.db.entity.DoubleRankMatchHistory;
-import com.tmp.xmash.dto.request.GameResultRequest;
+
 import java.util.List;
 
-public class DoubleMatchEvaluator {
+public class DoubleMatchEvaluator extends MatchEvaluator {
 
-    private final List<String> homeTeamIds;
-
-    private final List<String> awayTeamIds;
-
-    private final int homeScore;
-
-    private final int awayScore;
-
-    public DoubleMatchEvaluator(GameResultRequest gameResultRequest) {
-        this.homeTeamIds = gameResultRequest.homeTeam();
-        this.awayTeamIds = gameResultRequest.awayTeam();
-        this.homeScore = gameResultRequest.homeScore();
-        this.awayScore = gameResultRequest.awayScore();
+    public DoubleMatchEvaluator(List<String> homeTeamIds, List<String> awayUserIds, int homeScore, int awayScore) {
+        super(homeScore,
+                awayScore,
+                crateResultLp(homeScore, awayScore),
+                new Team(homeTeamIds.getFirst(), homeTeamIds.getLast()),
+                new Team(awayUserIds.getFirst(), awayUserIds.getLast()));
     }
 
-    public boolean isHomeWinner() {
-        return homeScore > awayScore;
+    @Override
+    public List<String> getUserIds() {
+        return List.of(homeTeam.userId1(), homeTeam.userId2(),
+                awayTeam.userId1(), awayTeam.userId2());
     }
 
-    public int getResultLp() {
-        int winnerScore = isHomeWinner() ? homeScore : awayScore;
+    @Override
+    public List<AppUser> getHomeUser(List<AppUser> matchUsers) {
+        return matchUsers.stream()
+                .filter(user -> homeTeam.userId1().equals(user.getUserId()) ||
+                        homeTeam.userId2().equals(user.getUserId()))
+                .toList();
+    }
 
-        if (winnerScore <= 11) {
-            return DEFAULT_WINNER_LP;
-        }
-
-        if (winnerScore <= 20) {
-            return DEFAULT_WINNER_LP + RANDOM_GENERATOR.nextInt(5);
-        }
-
-        return DEFAULT_WINNER_LP + (RANDOM_GENERATOR.nextInt(5) * 2);
+    @Override
+    public List<AppUser> getAwayUser(List<AppUser> matchUsers) {
+        return matchUsers.stream()
+                .filter(user -> awayTeam.userId1().equals(user.getUserId()) ||
+                        awayTeam.userId2().equals(user.getUserId()))
+                .toList();
     }
 
     public DoubleRankMatchHistory resolveRankMatchWinner()  {
         if (homeScore > awayScore) {
             return DoubleRankMatchHistory.builder()
-                    .winner1Id(homeTeamIds.get(0))
-                    .winner2Id(homeTeamIds.get(1))
+                    .winner1Id(homeTeam.userId1())
+                    .winner2Id(homeTeam.userId2())
                     .winnerScore(homeScore)
-                    .loser1Id(awayTeamIds.get(0))
-                    .loser2Id(awayTeamIds.get(1))
+                    .loser1Id(awayTeam.userId1())
+                    .loser2Id(awayTeam.userId2())
                     .loserScore(awayScore)
+                    .lp(resultLp)
                     .build();
         }
 
         return DoubleRankMatchHistory.builder()
-                .winner1Id(awayTeamIds.get(0))
-                .winner2Id(awayTeamIds.get(1))
+                .winner1Id(awayTeam.userId1())
+                .winner2Id(awayTeam.userId2())
                 .winnerScore(awayScore)
-                .loser1Id(homeTeamIds.get(0))
-                .loser2Id(homeTeamIds.get(1))
+                .loser1Id(homeTeam.userId1())
+                .loser2Id(homeTeam.userId2())
                 .loserScore(homeScore)
+                .lp(resultLp)
                 .build();
     }
 }
--- be/xmash/src/main/java/com/tmp/xmash/service/game/Impl/DoubleGameService.java
+++ be/xmash/src/main/java/com/tmp/xmash/service/game/Impl/DoubleGameService.java
@@ -3,85 +3,126 @@ package com.tmp.xmash.service.game.Impl;
 import static java.util.function.UnaryOperator.identity;
 import static java.util.stream.Collectors.toMap;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import com.tmp.xmash.domain.RequestUserRanking;
+import com.tmp.xmash.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.tmp.xmash.db.entity.AppUser;
 import com.tmp.xmash.db.entity.DoubleRankMatchHistory;
-import com.tmp.xmash.db.entity.UserRanking;
 import com.tmp.xmash.db.repositroy.DoubleRankMatchRepo;
-import com.tmp.xmash.db.repositroy.UserRepository;
 import com.tmp.xmash.domain.DoubleMatchEvaluator;
-import com.tmp.xmash.dto.request.GameResultRequest;
+import com.tmp.xmash.domain.MatchEvaluator;
+import com.tmp.xmash.dto.request.GameModifyRequest;
 import com.tmp.xmash.dto.response.GameResultResponse;
+import com.tmp.xmash.exption.BadRequestException;
 import com.tmp.xmash.service.RankingService;
 import com.tmp.xmash.service.game.GamePostAble;
 import com.tmp.xmash.service.game.GameService;
 import com.tmp.xmash.type.MatchType;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import com.tmp.xmash.util.XmashTimeCreator;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class DoubleGameService implements GameService, GamePostAble {
 
-    private final static MatchType matchType = MatchType.DOUBLE;
+    private static final MatchType MATCH_TYPE = MatchType.DOUBLE;
 
     private final DoubleRankMatchRepo doubleRankMatchRepo;
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final RankingService rankingService;
 
 
     @Transactional
     @Override
-    public boolean matchDone(GameResultRequest gameResultRequest)  {
-        checkScore(gameResultRequest);
+    public boolean matchDone(MatchEvaluator matchEvaluator)  {
+        DoubleMatchEvaluator doubleMatchEvaluator = (DoubleMatchEvaluator) matchEvaluator;
+        matchEvaluator.checkScore();
 
-        List<String> homeTeam = gameResultRequest.homeTeam();
-        List<String> awayTeam = gameResultRequest.awayTeam();
-        Set<String> userIds = Stream.concat(homeTeam.stream(), awayTeam.stream())
-                .collect(Collectors.toSet());
-        Set<AppUser> matchUsers = userRepository.findByUserIdIn(userIds);
-        Set<AppUser> homeUsers = matchUsers.stream()
-                .filter(a -> homeTeam.contains(a.getUserId()))
-                .collect(Collectors.toSet());
-        Set<AppUser> awayUser = matchUsers.stream()
-                .filter(a -> awayTeam.contains(a.getUserId()))
-                .collect(Collectors.toSet());
-        Set<UserRanking> homeSeasonRanking = homeUsers.stream()
-                .map(AppUser::getCurrentUserRanking)
-                .collect(Collectors.toSet());
-        Set<UserRanking> awaySeasonRanking =  awayUser.stream()
-                .map(AppUser::getCurrentUserRanking)
-                .collect(Collectors.toSet());
+        List<AppUser> matchUsers = userService.findByUserIdIn(doubleMatchEvaluator.getUserIds());
+        RequestUserRanking requestUserRanking = getRequestUserRanking(doubleMatchEvaluator, matchUsers);
+        doubleRankMatchRepo.save(doubleMatchEvaluator.resolveRankMatchWinner());
 
-        DoubleMatchEvaluator matchEvaluator = new DoubleMatchEvaluator(gameResultRequest);
-        DoubleRankMatchHistory singleMatchHistory = matchEvaluator.resolveRankMatchWinner();
-        doubleRankMatchRepo.save(singleMatchHistory);
-
-        if (matchEvaluator.isHomeWinner()) {
-            rankingService.updateRanking(homeSeasonRanking, awaySeasonRanking, matchEvaluator.getResultLp());
-
+        if (doubleMatchEvaluator.isHomeWinner()) {
+            rankingService.updateRanking(requestUserRanking.winnerRankings(), requestUserRanking.loserRankings(), doubleMatchEvaluator.getResultLp());
             return true;
         }
 
-        rankingService.updateRanking(awaySeasonRanking, homeSeasonRanking, matchEvaluator.getResultLp());
+        rankingService.updateRanking(requestUserRanking.winnerRankings(), requestUserRanking.loserRankings(), doubleMatchEvaluator.getResultLp());
         return false;
+    }
 
+    @Override
+    public void modifyMatchHistory(GameModifyRequest gameModifyRequest, long matchId) {
+        LocalDateTime tenMinutesAgo = XmashTimeCreator.getCurrentTimeUTC().minusMinutes(10);
+    
+        if (gameModifyRequest.matchTime().isBefore(tenMinutesAgo)) {
+            throw new BadRequestException("10분 이전 데이터는 수정할 수 없습니다.");
+        }
+        DoubleRankMatchHistory matchHistory = doubleRankMatchRepo.findById(matchId).orElseThrow();
+        int prevLp = matchHistory.getLp();
+
+        //Ranking 원복
+        resetRanking(gameModifyRequest, prevLp);
+        //MatchHistory 수정 값으로 update
+        DoubleMatchEvaluator doubleMatchEvaluator = updateMatchHistory(gameModifyRequest, matchHistory);
+        //Ranking 수정한 값에 따라 변동
+        updateRanking(prevLp, doubleMatchEvaluator);
+    }
+
+    private void resetRanking(GameModifyRequest gameModifyRequest, int prevLp) {
+        DoubleMatchEvaluator doubleMatchEvaluator = new DoubleMatchEvaluator(
+                gameModifyRequest.prevWinnerUserIds(),
+                gameModifyRequest.prevLoserUserIds(),
+                0,
+                0
+        );
+        List<AppUser> matchUsers = userService.findByUserIdIn(doubleMatchEvaluator.getUserIds());
+        RequestUserRanking prevRequestUserRanking = getRequestUserRanking(doubleMatchEvaluator, matchUsers);
+        rankingService.updateRanking(prevRequestUserRanking.winnerRankings(), prevRequestUserRanking.loserRankings(), prevLp * -1);
+    }
+
+    private DoubleMatchEvaluator updateMatchHistory(GameModifyRequest gameModifyRequest, DoubleRankMatchHistory matchHistory) {
+        //Match History Update
+        DoubleMatchEvaluator doubleMatchEvaluator = new DoubleMatchEvaluator(
+                gameModifyRequest.homeUserIds(),
+                gameModifyRequest.awayUserIds(),
+                gameModifyRequest.homeScore(),
+                gameModifyRequest.awayScore()
+        );
+        DoubleRankMatchHistory doubleRankMatchHistory = doubleMatchEvaluator.resolveRankMatchWinner();
+        matchHistory.updateMatchHistory( //기존 history 신규 객체로 Update
+                doubleRankMatchHistory.getWinner1Id(),
+                doubleRankMatchHistory.getWinner2Id(),
+                doubleRankMatchHistory.getLoser1Id(),
+                doubleRankMatchHistory.getLoser2Id(),
+                doubleRankMatchHistory.getWinnerScore(),
+                doubleRankMatchHistory.getLoserScore()
+        );
+
+        return doubleMatchEvaluator;
+    }
+
+    private void updateRanking(int prevLp, DoubleMatchEvaluator doubleMatchEvaluator) {
+        List<AppUser> matchUsers = userService.findByUserIdIn(doubleMatchEvaluator.getUserIds());
+        RequestUserRanking newRequestUserRanking = getRequestUserRanking(doubleMatchEvaluator, matchUsers);
+        rankingService.updateRanking(newRequestUserRanking.winnerRankings(), newRequestUserRanking.loserRankings(), prevLp);
     }
 
     @Override
     public List<GameResultResponse> getMatchHistory() {
-        Map<String, AppUser> usersByUserId = userRepository.findAll().stream()
+        Map<String, AppUser> usersByUserId = userService.findAll().stream()
                 .collect(toMap(AppUser::getUserId, identity()));
         List<DoubleRankMatchHistory> singleMatchHistories = doubleRankMatchRepo.findTop1000ByOrderByMatchTimeDesc();
 
         return singleMatchHistories.stream()
-                .map(matchHistory -> GameResultResponse.createDoubleRankGame(matchHistory, usersByUserId, matchType))
+                .map(matchHistory -> GameResultResponse.createDoubleRankGame(matchHistory, usersByUserId, MATCH_TYPE))
                 .toList();
     }
-
 }
