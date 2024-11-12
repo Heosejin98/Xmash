de071cb927d06240263ffad593f8f8f246f2dd98
김진범 <lljrwq123@gmail.com>
2024-11-12 23:29:02 +0900

    fix: api 주소 변경 game -> games

--- fe/src/shared/api/game/game.service.ts
+++ fe/src/shared/api/game/game.service.ts
@@ -1,19 +1,15 @@
-import { AxiosContracts } from "@/shared/lib/axios"
-import { api } from ".."
-import { CreateGameDto, GameDto, GameParamsQueryDto } from "./game.contracts"
-import { z } from "zod"
-
+import { AxiosContracts } from "@/shared/lib/axios";
+import { api } from "..";
+import { CreateGameDto, GameDto, GameParamsQueryDto } from "./game.contracts";
+import { z } from "zod";
 
 export class GameService {
   static gameMutation(data: { createGameDto: CreateGameDto }) {
-    const createGameDto = AxiosContracts.requestContract(
-      CreateGameDto,
-      data.createGameDto,
-    )
-    return api.post(`/game`, { ...createGameDto })
+    const createGameDto = AxiosContracts.requestContract(CreateGameDto, data.createGameDto);
+    return api.post(`/games`, { ...createGameDto });
   }
 
-  static gameQuery(config: { params: GameParamsQueryDto, signal?: AbortSignal }) {
-    return api.get(`/game`, config).then(AxiosContracts.responseContract(z.array(GameDto)))
+  static gameQuery(config: { params: GameParamsQueryDto; signal?: AbortSignal }) {
+    return api.get(`/games`, config).then(AxiosContracts.responseContract(z.array(GameDto)));
   }
 }
