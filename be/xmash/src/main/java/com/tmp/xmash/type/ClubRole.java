977858f39afe8c6b6c4d3a2897af7937aa25b19f
Heosejin98 <sejin3319@naver.com>
2024-11-27 00:59:07 +0900

    refactor: 미사용 함수 어노테이션& 함수 제거

--- be/xmash/src/main/java/com/tmp/xmash/type/ClubRole.java
+++ be/xmash/src/main/java/com/tmp/xmash/type/ClubRole.java
@@ -1,11 +1,8 @@
 package com.tmp.xmash.type;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public enum ClubRole {
     @Schema(description = "동호회 회원")
@@ -15,15 +12,5 @@ public enum ClubRole {
     NON_MEMBER,
 
     @Schema(description = "임시 회원")
-    GUEST;
-
-    @JsonCreator
-    public static ClubRole fromValue(String value) {
-        return switch (value) {
-            case "MEMBER" -> ClubRole.MEMBER;
-            case "NON_MEMBER" -> ClubRole.NON_MEMBER;
-            case "GUEST" -> ClubRole.GUEST;
-            default -> throw new IllegalArgumentException("Unknown value " + value);
-        };
-    }
+    GUEST
 }
