4768e611ec740f8ed7e6fc73674a26b23c3756a6
Heosejin98 <sejin3319@naver.com>
2024-11-27 01:51:46 +0900

    refactor: 필요한 변환 함수만 남김 - 필요 할 것 같아서 만들어 둔 enum value 삭제 남, 여 - RankGame 삭제로 인한 GameType 삭제 - Req, Rep 시 변환을 위한 method만 남김

--- be/xmash/src/main/java/com/tmp/xmash/web/editor/MatchTypeEditor.java
+++ be/xmash/src/main/java/com/tmp/xmash/web/editor/MatchTypeEditor.java
@@ -4,14 +4,12 @@ import java.beans.PropertyEditorSupport;
 
 public class MatchTypeEditor extends PropertyEditorSupport {
 
+    /**
+     * Request에서 사용됨: 클라이언트로부터 받은 문자열을 대문자로 변환하여 설정
+     * "single"이라는 문자열이 들어오면 "SINGLE"로 변환됩니다.
+     */
     @Override
     public void setAsText(String text) throws IllegalArgumentException {
         setValue(text.toUpperCase());
     }
-
-    @Override
-    public String getAsText() {
-        return ((String) getValue()).toLowerCase();
-    }
-
 }
